eeb57b7992d8f9e24109edcf5b8f52f43ed9fd4d
nadinesk <nadine.khattak@gmail.com>
2017-08-30 21:24:58 -0400

    added jwt auth and tests and passing all test

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::AuthController < ApplicationController 
+  before_action :authenticate_token!, only: [:refresh]
+
+  def login
+    @user = User.find_by(email: params[:user][:email])
+    if !@user 
+      render json: {
+        errors: {
+          email: ["Unable to find a user with the provided email address"]
+        }
+      }, status: 500
+    elsif @user && @user.authenticate(params[:user][:password]) 
+      render 'users/user_with_token.json.jbuilder', user: @user
+    else 
+      render json: {
+        errors: {
+          password: ["Password does not match the provided email"]
+        }
+      }, status: 500
+    end
+  end
+
+  def refresh 
+    render 'users/user_with_token.json.jbuilder', user: @user
+  end
+
+end
---
+++ spec/requests/api/v1/auth_request_spec.rb
@@ -0,0 +1,128 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users", type: :request do 
+
+  before(:each) do  
+    @user = User.create(
+      email: 'nadine@blah.com', 
+      username: 'nadine',
+      password: 'password'
+    )
+  end
+
+  describe "POST /auth" do 
+
+    describe "on success" do 
+
+      before(:each) do  
+        params = { 
+          user: { 
+            email: 'nadine@blah.com', 
+            password: 'password' 
+          } 
+        }
+
+        post "/api/v1/auth", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+          
+        @response = response
+      end
+
+      it "returns the existing user (from params) and a JWT token" do  
+        body = JSON.parse(@response.body)
+        binding.pry
+        expect(@response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq('nadine@blah.com')
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)  
+      end
+    end
+
+    describe "on error" do
+
+      it "unable to find user with email" do  
+        params = { 
+          user: { 
+            email: 'luke@the_pub.com', 
+            password: 'password' 
+          } 
+        }
+
+        post "/api/v1/auth", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+          
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(body["errors"]).to eq({
+          "email"=>["Unable to find a user with the provided email address"]
+        })
+      end
+
+      it "password does not match the provided email" do  
+        params = { 
+          user: { 
+            email: 'nadine@blah.com', 
+            password: 'monkeys' 
+          } 
+        }
+
+        post "/api/v1/auth", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+          
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(body["errors"]).to eq({
+          "password"=>["Password does not match the provided email"]
+        })
+      end
+    end
+  end
+
+  describe "POST /auth/refresh" do  
+
+     describe "on success" do 
+
+      it "returns the existing user (from the headers JWT) and a new JWT token" do  
+        token = Auth.create_token(@user.id)        
+
+        post "/api/v1/auth/refresh", 
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer: #{token}"
+          }
+          
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq('nadine@blah.com')
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)  
+      end
+    end
+
+    describe "on error" do
+
+      it "unable to find user with email" do  
+        token = 'abc.123.def.456'     
+
+        post "/api/v1/auth/refresh", 
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer: #{token}"
+          }
+          
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(403)
+        expect(body["errors"]).to eq([{ "message" => "Token is invalid!" }])
+      end
+    end
+  end
+end
