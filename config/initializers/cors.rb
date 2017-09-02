31139dca5f08ba19ecbc4dd49d4e083508d9662c
nadinesk <nadine.khattak@gmail.com>
2017-09-02 05:53:47 -0400

    another cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins *
+    origins 'https://localhost:3000.com', 'https://good-decision.herokapp.com'
 
-    resource '/*',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
