3a6c9e56ba4a1e53462458d6150dfaa025a2916c
nadinesk <nadine.khattak@gmail.com>
2017-09-02 05:33:25 -0400

    change cors.rb again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://good-decision.herokapp.com'
+    origins 'https://localhost:3000.com', 'https://good-decision.herokapp.com'
 
     resource '*',
       headers: :any,
