07f7577b82d3db1c6406036c7869f15926a12f1a
nadinesk <nadine.khattak@gmail.com>
2017-09-04 07:58:53 -0400

    fixed params 1

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -7,82 +7,11 @@ class Api::V1::UsersController < ApplicationController
 
   end
 
-  def show
-   		@decision = Decision.find_by(id: params[:id])
-   		if @decision 
-   			render 'decisions/decision.json.jbuilder', decision: @decision
-   		else 
-   			render json: {
-   				errors: {
-   					message: "Page not found"
-   				}
-   			}, status: 404
-   		end
-      
-   end
-
   
-   def create
-   		@user = User.find_by(id: params[:user_id])
-   		if @user.id == current_user.id
-   			@decision = @user.decisions.new(decision_params)
-   			if @decision.save
-   				render '/decisions/decision.json.jbuilder', decision: @decision
-   			else 
-   				render json: {
-   					errors: @decision.errors.full_messages
-   				}, status: 500
-   			end
-   		else 
-   			render json: {
-   				errors: [
-   					{message: "No permission to add decisions"}
-   				]
-   			}, status: 403
-   		end
-
-   end
-
-   def update 
-   		@user = User.find_by(id: params[:user_id])
-   		if @user.id == current_user.id
-   			@decision = @user.decisions.find_by(id: params[:id])
-   			if @decision.update_attributes(decision_params) 
-   				render 'decisions/decision.json.jbuilder', decision: @decision
-   			else 
-   				render json: {
-   					errors: @decision.errors.full_messages
-   				}, status: 500
-   			end
-   		else 
-   			render json: {
-   				errors: [
-   					{message: "You don't have permission to edit"}
-   				]
-   			}, status: 403
-   		end
-   end
-
-   def destroy
-   		decision = Decision.find_by(id: params[:id])
-   		if decision
-   			decision.destroy
-   			head :no_contnet
-   		else
-   			render json: {
-   				errors: {
-   					message: "Page not found"
-   				}
-   			}, status: 404
-   		end
-
-   end
 
   private 
 
-    def decision_params 
-      params.require(:decision).permit(:title)
-    end
+   
     
 end
 end
