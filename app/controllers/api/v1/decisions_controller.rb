15a6feda72e8dfeb96bdb749dacfa247fbc8ff4a
nadinesk <nadine.khattak@gmail.com>
2017-09-04 08:09:23 -0400

    added index to decision

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::DecisionsController < ApplicationController
  
   
 
-  
+   def index
+
+	    @decisions = User.find_by(id: params[:user_id])&.decisions
+    	render 'decisions/decisions.json.jbuilder', decisions: @decisions
+
+  end
 
    
     
