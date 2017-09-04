1899420435c00b4633f0c76d263f84b0fda64b91
nadinesk <nadine.khattak@gmail.com>
2017-09-04 08:04:55 -0400

    fixed params 1

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::DecisionsController < ApplicationController 
   
-   def index
-
-	    @decisions = User.find_by(id: params[:user_id])&.decisions
-    	render 'decisions/decisions.json.jbuilder', decisions: @decisions
-
-  end
-
+ 
   
 
   private 
